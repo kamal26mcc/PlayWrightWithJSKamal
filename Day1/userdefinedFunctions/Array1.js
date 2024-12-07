24df271aa7399f88e6de4fcc70251323cfbb6954
kamal26mcc <kamal26mcc@gmail.com>
2024-12-07 16:44:31 +0530

    proper alignment

--- Day1/userdefinedFunctions/Array1.js
+++ Day1/userdefinedFunctions/Array1.js
@@ -9,20 +9,20 @@ ar[1]="C";
 ar[2]=true;
 ar[3]=24;
 */
-var ar=["B","C",true,24]
+var ar = ["B", "C", true, 24]
 
-console.log("size="+ar.length)
+console.log("size=" + ar.length)
 //console.log(ar[2])
 
 console.log("---------ForLoop-----------")
-for(let i=0;i<ar.length;i++){
+for (let i = 0; i < ar.length; i++) {
     console.log(ar[i])
 }
 
 console.log("---------in operator-----------")
-for(var p in ar){
+for (var p in ar) {
     console.log(ar[p])
 }
 
 console.log("---------forEachLoop-----------")
-ar.forEach(num=>console.log(num))
+ar.forEach(num => console.log(num))
